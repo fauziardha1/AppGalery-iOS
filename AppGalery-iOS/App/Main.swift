236444ab116a96c5db64b514f46cebfb157c1eee
fauziardha1 <fauziardasaputra@gmail.com>
2023-02-11 22:43:14 +0700

    init project showcase

---
+++ AppGalery-iOS/App/Main.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppGalery-iOS
+//
+//  Created by FauziArda on 11/02/23.
+//
+
+import UIKit
+
+class Main: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
